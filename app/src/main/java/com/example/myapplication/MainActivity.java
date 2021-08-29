87ceb30082221f4dfd0c55446dd427dfcfd46ebd
Aritra995 <aritrasiliguri@gmail.com>
2021-08-29 20:45:38 +0530

    Development env factored

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import android.nfc.Tag;
+import android.util.Log;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.databinding.DataBindingUtil;
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.google.firebase.database.*;
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference myref = firebaseDatabase.getReference();
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        myref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                String value = snapshot.getValue(String.class);
+                TextView textView = findViewById(R.id.text2);
+                textView.setText(value.toString());
+                Log.d(TAG,"Value is: "+value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.w(TAG,"Failed to read value: ",error.toException());
+            }
+        });
+//        myref.setValue("Hello from java application");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
