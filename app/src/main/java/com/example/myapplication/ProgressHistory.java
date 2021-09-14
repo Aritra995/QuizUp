aaa5e038b9f882ea33db7847d8a7aa7dc7bf4b56
Aritra995 <aritrasiliguri@gmail.com>
2021-09-14 23:07:50 +0530

    Progress card designed

--- app/src/main/java/com/example/myapplication/ProgressHistory.java
+++ app/src/main/java/com/example/myapplication/ProgressHistory.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
