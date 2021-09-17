919601fcf59ce507febf2c84016b420fb3a7099f
Aritra995 <aritrasiliguri@gmail.com>
2021-09-17 17:05:39 +0530

    Feedback activity UI designed

--- app/src/main/java/com/example/myapplication/StudentsPortalActivity.java
+++ app/src/main/java/com/example/myapplication/StudentsPortalActivity.java
@@ -115,6 +115,10 @@ public class StudentsPortalActivity extends AppCompatActivity implements Navigat
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
                 return true;
+            case R.id.feedback:
+                intent = new Intent(StudentsPortalActivity.this,FeedbackActivity.class);
+                startActivity(intent);
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
